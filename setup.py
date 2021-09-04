ea5f68a406ec3be99bf2799ad947a16b6a18a4e6
0xFable <0xfable@protonmail.com>
2021-09-04 20:59:10 +0100

    chore(deps): Add toml to deps

--- setup.py
+++ setup.py
@@ -15,5 +15,9 @@ setup(
     author="0xFable",
     author_email="0xfable@protonmail.com",
     description="Capsule is small Python SDK tool you can use to deploy Terra CosmWasm contracts to a given Terra (Cosmos SDK Based) chain.",
+    install_requires=[
+        'argparse',
+        'toml'
+    ],
     keywords="cosmwasm terra",
 )
