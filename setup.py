493e84f62965b62de19bace0ad2c7a327a6590cf
0xFable <0xfable@protonmail.com>
2021-09-05 22:54:14 +0100

    feat(version): Bump version and include readme in Pypi text

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
 
+from os import path
+import io
+this_directory = path.abspath(path.dirname(__file__))
+
+with io.open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    readme_text = f.read()
+    long_description = readme_text
+
+
 setup(
     name="capsule_terra",
     license="MIT",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     include_package_data=True,
     # Add command line: capsule and terra_deploy
@@ -16,6 +25,8 @@ setup(
     author="0xFable",
     author_email="0xfable@protonmail.com",
     description="Capsule is small Python SDK tool you can use to deploy Terra CosmWasm contracts to a given Terra (Cosmos SDK Based) chain.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'argparse',
         'requests',
