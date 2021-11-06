00b981ccae3dbfcb1f14d55e7c4c970b537a41fa
0xFable <0xfable@protonmail.com>
2021-11-06 12:49:59 +0000

    chore(setup): Update setup.py

--- setup.py
+++ setup.py
@@ -10,21 +10,21 @@ with io.open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 
 setup(
-    name="capsule_terra",
-    license="MIT",
-    version="0.0.2",
-    packages=find_packages(),
+    name='capsule_terra',
+    license='MIT',
+    version='0.0.2',
+    packages=find_packages(include=['capsule', 'capsule.*']),
     include_package_data=True,
     # Add command line: capsule and terra_deploy
     entry_points={
-        "console_scripts": ["capsule=capsule.cli:main",
-        "terra_deploy=capsule.cli:main"]
+        'console_scripts': ['capsule=capsule.cli:main',
+        'terra_deploy=capsule.cli:main']
     },
 
     # PyPI metadata
-    author="0xFable",
-    author_email="0xfable@protonmail.com",
-    description="Capsule is small Python SDK tool you can use to deploy Terra CosmWasm contracts to a given Terra (Cosmos SDK Based) chain.",
+    author='0xFable',
+    author_email='0xfable@protonmail.com',
+    description='Capsule is small Python SDK tool you can use to deploy Terra CosmWasm contracts to a given Terra (Cosmos SDK Based) chain.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
@@ -34,5 +34,5 @@ setup(
         'toml',
         'GitPython'
     ],
-    keywords="cosmwasm terra",
+    keywords='cosmwasm terra',
 )
