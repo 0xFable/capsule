fbb5ee05495e63b9d0ab3d4eddbfc7393d15ad77
0xFable <0xfable@protonmail.com>
2021-11-06 16:07:09 +0000

    feat(execute): Add command for Executing contracts on a given chain
    resovles #7

--- capsule/cmds/__init__.py
+++ capsule/cmds/__init__.py
@@ -1,4 +1,6 @@
 from .deploy import DeployCmd
 from .local import LocalCmd
 from .query import QueryCmd
-AVAILABLE_COMMANDS:list = [DeployCmd, LocalCmd, QueryCmd]
+from .execute import ExecuteCmd
+
+AVAILABLE_COMMANDS:list = [DeployCmd, LocalCmd, QueryCmd, ExecuteCmd]
